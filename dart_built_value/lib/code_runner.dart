ff196b6d4c21ef12fbb891ece71515a3e2ef054e
cuong.duong <cuong.duong@titancorpvn.com>
2019-03-13 17:21:04 +0700

    null field error

--- dart_built_value/lib/code_runner.dart
+++ dart_built_value/lib/code_runner.dart
@@ -3,20 +3,19 @@ import 'package:dart_built_value/model/built_vehicle.dart';
 class CodeRunner {
   static void runCode() {
     var car = BuiltVehicle((b) => b..type = 'car'
-    ..brand = 'Tesla'
-    ..price= 100000);
-
-    var car2 = BuiltVehicle((b) => b..type = 'car'
-    ..brand = 'Tesla'
     ..price= 100000);
 
     var copiedCar = car.rebuild((b) => b..brand = 'BMW'); // rebuild to create new instance, not change 'car'
 
     print(car);
     print(copiedCar);
-    print(car == car2); // true
 
     // They are not of the same object, they don't hold same reference to the memory
     // But in the case of value classed, these two objects are the same because they hold the same data
+
+/* ══╡ EXCEPTION CAUGHT BY GESTURE ╞═══════════════════════════════════════════════════════════════════
+The following BuiltValueNullFieldError was thrown while handling a gesture:
+Tried to construct class "BuiltVehicle" with null field "brand". This is forbidden; to allow it,
+mark "brand" with @nullable. */
   }
 }
