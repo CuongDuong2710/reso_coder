5bada79d74f420ed4617f1e3de764bfedee2aaa0
cuong.duong <cuong.duong@titancorpvn.com>
2019-03-14 14:57:25 +0700

    add plugin

--- dart_built_value/lib/model/serializers.dart
+++ dart_built_value/lib/model/serializers.dart
@@ -6,6 +6,7 @@ import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
 import 'package:dart_built_value/model/built_vehicle.dart';
+import 'package:built_value/standard_json_plugin.dart';
 
 part 'serializers.g.dart';
 
@@ -13,4 +14,4 @@ part 'serializers.g.dart';
   BuiltVehicle,
   VehicleType
 ])
-final Serializers serializers = _$serializers;
+final Serializers serializers = (_$serializers.toBuilder()..addPlugin(StandardJsonPlugin())).build();
