8a71d953ac31d714766d77370f4e94d1ad04ef00
cuongdnq2710 <cuong.duong@titancorpvn.com>
2019-03-18 20:59:16 +0700

    fetch next result page

--- youtube_search/lib/repository/youtube_repository.dart
+++ youtube_search/lib/repository/youtube_repository.dart
@@ -5,15 +5,54 @@ import 'package:youtube_search/network/youtube_data_source.dart';
 class YouTubeRepository {
   YouTubeDataSource _youTubeDataSource;
 
+  String _lastSearchQuery;
+  String _nextPageToken;
+
   YouTubeRepository(this._youTubeDataSource);
 
   Future<BuiltList<SearchItem>> searchVideos(String query) async {
     final searchResult = await _youTubeDataSource.searchVideos(query: query);
+
+    // cache last query & next page token
+    _cacheValues(nextPageToken: searchResult.nextPageToken, query: query);
+
     if (searchResult.items.isEmpty) throw NoSearchResultException();
+
     return searchResult.items;
+  }
+
+  void _cacheValues({String query, String nextPageToken}) {
+    _lastSearchQuery = query;
+    _nextPageToken = nextPageToken;
+  }
+
+  Future<BuiltList<SearchItem>> fetchNextResultPage() async {
+    if (_lastSearchQuery == null) {
+      throw NoSearchResultException();
+    }
+
+    if (_nextPageToken == null) {
+      throw NoNextPageTokenException();
+    }
+
+    final nextPageSearchResult = await _youTubeDataSource.searchVideos(
+        query: _lastSearchQuery, pageToken: _nextPageToken);
+
+    // save cache values
+    _cacheValues(
+        query: _lastSearchQuery,
+        nextPageToken: nextPageSearchResult.nextPageToken);
+
+    return nextPageSearchResult.items;
   }
 }
 
 class NoSearchResultException implements Exception {
   final message = 'No results';
 }
+
+class SearchNotInitiatedException implements Exception {
+  final message = 'Cannot get the next result page without searching first';
+}
+
+class NoNextPageTokenException implements Exception {}
