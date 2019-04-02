0c89fc343157e795a513851a18df30beccde628b
cuongdnq2710 <cuong.duong@titancorpvn.com>
2019-04-02 22:08:21 +0700

    add YoutubeRepository in constructor

--- youtube_search/lib/ui/search/search_bloc.dart
+++ youtube_search/lib/ui/search/search_bloc.dart
@@ -1,7 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:youtube_search/ui/search/search.dart';
+import 'package:youtube_search/repository/youtube_repository.dart';
 
 class SearchBloc extends Bloc<SearchEvent, SearchState> {
+  final YouTubeRepository _youtubeRepository;
+
+  SearchBloc(this._youtubeRepository) : super();
+
   void onSearchInitiated(String query) {
     dispatch(SearchInitiated((b) => b..query = query));
   }
@@ -15,3 +20,5 @@ class SearchBloc extends Bloc<SearchEvent, SearchState> {
     // TODO: implement mapEventToState
   }
 }
+
+// whenever we yield something from an async generator, adding one thing to the stream.
