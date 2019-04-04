07f46c6a1b235e58dd869a78c48c6a8089b79dba
cuongdnq2710 <cuong.duong@titancorpvn.com>
2019-04-04 22:21:30 +0700

    create SearchPage and init kiwi

---
+++ youtube_search/lib/injection_container.dart
@@ -0,0 +1,13 @@
+import 'package:kiwi/kiwi.dart' as kiwi;
+import 'package:http/http.dart' as http;
+import 'package:youtube_search/network/youtube_data_source.dart';
+import 'package:youtube_search/repository/youtube_repository.dart';
+import 'package:youtube_search/ui/search/search_bloc.dart';
+
+void initKiwi() {
+  kiwi.Container()
+    ..registerInstance(http.Client())
+    ..registerFactory((container) => YouTubeDataSource(container.resolve()))
+    ..registerFactory((container) => YouTubeRepository(container.resolve()))
+    ..registerFactory((container) => SearchBloc(container.resolve()));
+}
