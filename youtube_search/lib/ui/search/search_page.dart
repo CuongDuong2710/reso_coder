5be1a995548d6980d0640e9a57b98a58fa25ba42
cuongdnq2710 <cuong.duong@titancorpvn.com>
2019-04-12 00:07:24 +0700

    buildResultList with NotificationListener

--- youtube_search/lib/ui/search/search_page.dart
+++ youtube_search/lib/ui/search/search_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kiwi/kiwi.dart' as kiwi;
+import 'package:youtube_search/model/search/search_snippet.dart';
 import 'package:youtube_search/ui/search/search_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:youtube_search/ui/search/search_state.dart';
@@ -49,11 +50,61 @@ class _SearchPageState extends State<SearchPage> {
               );
             }
             if (state.isSuccessful) {
+              return _buildResultList(state);
             } else {
               return CenteredMessages(
                   message: state.error, icon: Icons.error_outline);
             }
           }),
+    );
+  }
+
+  Widget _buildResultList(SearchState state) {
+    return NotificationListener<ScrollNotification>(
+        onNotification: _handleScrollNotification, // as function reference
+        child: ListView.builder(
+          itemCount: _calculateListItemCount(state), // call function
+          controller: _scrollController,
+          itemBuilder: (context, index) {
+            return index >= state.searchResults.length
+                ? _buildLoaderListItem()
+                : _buildVideoListItemCard(state.searchResults[index].snippet);
+          },
+        ));
+  }
+
+  int _calculateListItemCount(SearchState state) {
+    if (state.hasReachedEndOfResults)
+      return state.searchResults.length;
+    else
+      //add one CircularProgressIndicator at bottom
+      return state.searchResults.length + 1;
+  }
+
+  bool _handleScrollNotification(ScrollNotification notification) {
+    if (notification is ScrollEndNotification &&
+        _scrollController.position.extentAfter == 0) {
+      // if that space below the last item
+      // inside of this view is zero -> in fact at the bottom of list view
+      _searchBloc.fetchNextResultPage();
+    }
+    return false;
+  }
+
+  Widget _buildLoaderListItem() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Widget _buildVideoListItemCard(SearchSnippet snippet) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[Image.network(snippet.thumbnails.high.url)],
+        ),
+      ),
     );
   }
 }
