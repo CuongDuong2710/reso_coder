44a63a7d101e11e568b20e230590c4bc09d1ec37
cuongdnq2710 <cuong.duong@titancorpvn.com>
2019-04-23 22:47:45 +0700

    injection DetailBloc/CustomScrollView with Silver

--- youtube_search/lib/injection_container.dart
+++ youtube_search/lib/injection_container.dart
@@ -2,6 +2,7 @@ import 'package:kiwi/kiwi.dart' as kiwi;
 import 'package:http/http.dart' as http;
 import 'package:youtube_search/network/youtube_data_source.dart';
 import 'package:youtube_search/repository/youtube_repository.dart';
+import 'package:youtube_search/ui/detail/detail_bloc.dart';
 import 'package:youtube_search/ui/search/search_bloc.dart';
 
 void initKiwi() {
@@ -9,5 +10,6 @@ void initKiwi() {
     ..registerInstance(http.Client())
     ..registerFactory((container) => YouTubeDataSource(container.resolve()))
     ..registerFactory((container) => YouTubeRepository(container.resolve()))
-    ..registerFactory((container) => SearchBloc(container.resolve()));
+    ..registerFactory((container) => SearchBloc(container.resolve()))
+    ..registerFactory((container) => DetailBloc(container.resolve()));
 }
--- youtube_search/lib/ui/detail/detail_page.dart
+++ youtube_search/lib/ui/detail/detail_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:kiwi/kiwi.dart' as kiwi;
+import 'package:youtube_search/ui/detail/detail.dart';
+import 'package:youtube_search/ui/detail/detail_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DetailPage extends StatefulWidget {
   final String videoId;
@@ -11,8 +15,47 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  final _detailBloc = kiwi.Container().resolve<DetailBloc>();
+
+  @override
+  void initState() {
+    super.initState();
+    _detailBloc.onShowDetail(id: widget.videoId);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _detailBloc.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocProvider(
+      bloc: _detailBloc,
+      child: Scaffold(
+        body: BlocBuilder(
+          bloc: _detailBloc,
+          builder: _buildScaffoldContent,
+        ),
+      ),
+    );
   }
+
+  Widget _buildScaffoldContent(BuildContext context, DetailState state) {
+    // CustomScrollView can hold multiple Silvers which have custom scrolling effects
+    // A Silver is simply a portion of a scrollable area.
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          expandedHeight: 178,
+          pinned: true,
+          flexibleSpace: _buildSilverAppBarContent(state),
+          // app bar stills be seen (red background) when we scroll down
+        )
+      ],
+    );
+  }
+
+  FlexibleSpaceBar _buildSilverAppBarContent(DetailState state) {}
 }
